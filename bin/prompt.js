a4e8dd3ba992fa64ceeacabcc1e8ba1e161d82d8
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-04-10 17:23:40 +0700

    chore: format files

--- bin/prompt.js
+++ bin/prompt.js
@@ -1,49 +1,49 @@
 const inquirer = require('inquirer');
 
 module.exports = async function initPrompt() {
-  const answer = await inquirer.prompt([
-    {
-      message: 'Type URL to be test? (e.g: https://mazipan.space)',
-      default: 'https://mazipan.space',
-      type: 'input',
-      name: 'url',
-    },
-    {
-      message: 'Choose device type?',
-      default: 'mobile',
-      type: 'list',
-			name: 'device',
-      choices: [
-        { name: 'mobile', disabled: false },
-        { name: 'desktop', disabled: false },
-      ],
-    },
-    {
-      message: 'How many times do you want to run the PSI?',
-      default: 1,
-      type: 'list',
-      choices: [
-        { name: 1, disabled: false },
-        { name: 3, disabled: false },
-        { name: 5, disabled: false },
-        { name: 7, disabled: false },
-        { name: 9, disabled: false },
-      ],
-      name: 'hit',
+	const answer = await inquirer.prompt([
+		{
+			message: 'Type URL to be test? (e.g: https://mazipan.space)',
+			default: 'https://mazipan.space',
+			type: 'input',
+			name: 'url',
 		},
 		{
-      message: 'Do you want to use custom quantile? (default is 0.75, range from 0 to 1)',
-      default: '0.75',
-      type: 'input',
-      name: 'quantile',
-    },
+			message: 'Choose device type?',
+			default: 'mobile',
+			type: 'list',
+			name: 'device',
+			choices: [
+				{ name: 'mobile', disabled: false },
+				{ name: 'desktop', disabled: false },
+			],
+		},
 		{
-      message: 'Type your API key? (check on https://s.id/apikey)',
-      default: '',
-      type: 'input',
-      name: 'apikey',
-    },
-  ]);
+			message: 'How many times do you want to run the PSI?',
+			default: 1,
+			type: 'list',
+			choices: [
+				{ name: 1, disabled: false },
+				{ name: 3, disabled: false },
+				{ name: 5, disabled: false },
+				{ name: 7, disabled: false },
+				{ name: 9, disabled: false },
+			],
+			name: 'hit',
+		},
+		{
+			message: 'Do you want to use custom quantile? (default is 0.75, range from 0 to 1)',
+			default: '0.75',
+			type: 'input',
+			name: 'quantile',
+		},
+		{
+			message: 'Type your API key? (check on https://s.id/apikey)',
+			default: '',
+			type: 'input',
+			name: 'apikey',
+		},
+	]);
 
-  return answer;
+	return answer;
 }
